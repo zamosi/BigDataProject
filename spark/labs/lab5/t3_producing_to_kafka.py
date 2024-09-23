4079dae7dd2dc1d076ba946072bff3b29c169176
zamosi <zamosi@gmail.com>
2024-09-23 14:29:06 +0000

    finish lab5-t3

--- spark/labs/lab5/t3_producing_to_kafka.py
+++ spark/labs/lab5/t3_producing_to_kafka.py
@@ -1,15 +1,26 @@
+import time
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from pyspark.sql import types as T
-from pyspark.sql import Row
+from kafka import KafkaProducer
 
-spark = SparkSession\
-    .builder\
-    .appName('EX5_t2')\
-    .master('local')\
-    .getOrCreate()
+spark = SparkSession.builder.master("local").appName('EX5_t3').getOrCreate()
 
 
 google_reviews_df = spark.read.parquet('s3a://data/source/google_reviews')
 
-google_reviews_df.show()
+data = google_reviews_df.toJSON()
+
+producer = KafkaProducer(bootstrap_servers='course-kafka:9092', value_serializer=lambda v: v.encode('utf-8'))
+
+i = 0
+for json_data in data.collect():
+    i = i + 1
+    producer.send(topic='gps-user-review-source', value=json_data)
+    if i == 50:
+        producer.flush()
+        time.sleep(5)
+        i = 0
+
+producer.close()
+spark.stop()
