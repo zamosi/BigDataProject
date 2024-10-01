56eb590d6ba0ec0d013ee9a472f5e46a06364943
zamosi <zamosi@gmail.com>
2024-10-01 20:35:39 +0000

    finish car t7

--- spark/Mid Project/cars/ex7_car.py
+++ spark/Mid Project/cars/ex7_car.py
@@ -7,7 +7,7 @@ from pyspark.sql import types as T
 spark = SparkSession \
  .builder \
  .master("local") \
- .appName('car_ex6') \
+ .appName('car_ex7') \
  .config('spark.jars.packages', 'org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2') \
  .getOrCreate()
 
@@ -27,7 +27,7 @@ stream_df = spark \
  .readStream \
  .format('kafka') \
  .option("kafka.bootstrap.servers", "course-kafka:9092") \
- .option("subscribe", "samples-enriched") \
+ .option("subscribe", "alert-data") \
  .option('startingOffsets', 'earliest') \
  .option("failOnDataLoss", "false") \
  .load() \
@@ -38,20 +38,22 @@ parsed_df = stream_df \
  .withColumn('parsed_json', F.from_json(F.col('value'), json_schema)) \
  .select(F.col('parsed_json.*'))
 
+df = parsed_df.agg(
+    F.count(F.col('event_id')).alias('num_of_rows'),
+    F.sum(F.when(F.col('color_name')==F.lit('Black'),1).otherwise(0)).alias('num_of_black'),
+    F.sum(F.when(F.col('color_name')==F.lit('White'),1).otherwise(0)).alias('num_of_white'),
+    F.sum(F.when(F.col('color_name')==F.lit('Silver'),1).otherwise(0)).alias('num_of_silver'),
+    F.max(F.col('speed')).alias('maximum_speed'),
+    F.max(F.col('gear')).alias('maximum_gear'),
+    F.max(F.col('rpm')).alias('maximum_rpm')
+    )
+                   
 
-filter_df = parsed_df.filter((parsed_df.speed>120) | (parsed_df.rpm>6000) | (parsed_df.expected_gear!=parsed_df.gear))
-# filter_df = parsed_df.filter((parsed_df.speed>120)&(parsed_df.rpm>6000))
 
-query = filter_df.selectExpr("to_json(struct(*)) AS value")\
- .writeStream \
- .format('kafka') \
- .option("kafka.bootstrap.servers", "course-kafka:9092") \
- .option("topic", "alert-data") \
- .option('startingOffsets', 'earliest')\
- .option('checkpointLocation', 's3a://spark/checkpoints/final_ex/alert-data') \
- .outputMode('append') \
- .start()
+query = df.writeStream \
+    .trigger(processingTime='15 minute') \
+    .outputMode("update") \
+    .format("console") \
+    .start()
 
 query.awaitTermination()
-
-spark.stop()
